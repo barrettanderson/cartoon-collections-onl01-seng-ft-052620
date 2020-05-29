fe54b3a7999e02dead3e308cc829b6dd440450d8
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 23:06:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,6 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese_types.find{cheese_types.include?(cheddar)} #true
+  cheddar_cheese.find{|i|cheese_types.include?(i)} #true
 
 end

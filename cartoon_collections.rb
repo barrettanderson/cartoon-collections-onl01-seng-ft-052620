0caca6368b7f823e9ca5744624dd5c7774daa914
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 22:34:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 
 def summon_captain_planet(veggies)
-  veggies.collect! {|name| name.capitalize }# Your code here
+  veggies.collect! {|name| name.capitalize + "!"}# Your code here
 end
 
 def long_planeteer_calls# code an argument here
